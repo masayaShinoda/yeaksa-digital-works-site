1744a72cc4219e07a614271ac577b8d0d0c3e171
Masaya Shida <masayashida@gmail.com>
2021-07-20 01:16:37 +0700

    removed svgr, added next-images

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
+const withImages = require('next-images')
+module.exports = withImages()
 module.exports = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-  }
 }
