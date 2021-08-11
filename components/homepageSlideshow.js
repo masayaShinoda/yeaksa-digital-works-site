401cd915eef473927b1da52207d0e50776ceafe5
Masaya Shida <masayashida@gmail.com>
2021-08-11 20:40:16 +0700

    removed unneeded pics, added tooltip

--- components/homepageSlideshow.js
+++ components/homepageSlideshow.js
@@ -5,7 +5,7 @@ export default function HomepageSlideshow() {
     return (
         <div className={styles.slideshow} id="slideshow">
             <div className={styles.slideWrapper}>
-                <div className={styles.slide + ` animate__animated animate__fadeInUp`}>
+                <div className={styles.slide + ` animate__animated animate__fadeInUp`} title="Brand identity works done by YeakSa Digital" >
                     
                 </div>
             </div>
