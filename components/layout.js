ab5371997df4b222da51dbcb5068b89afad28a51
Masaya Shida <masayashida@gmail.com>
2021-09-04 16:55:05 +0700

    ysdw updated seo

--- components/layout.js
+++ components/layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children, pageName }) {
         <div className={styles.container}>
             <Head>
                 <title>YeakSa Digital Works</title>
-                <meta name="description" content="Design agency based in Phnom Penh with an Esports niche." />
+                <meta name="description" content="Design agency based in Phnom Penh making businesses achieve a quality online presence." />
                 <link rel="icon" href="/icons/brand-identity-golden-ratio-08_result.ico" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
@@ -23,12 +23,12 @@ export default function Layout({ children, pageName }) {
             {pageName ? 
                 <NextSeo 
                     title={`${pageName} | YeakSa Digital Works`}
-                    description="Design agency based in Phnom Penh with an Esports niche."
+                    description="Design agency based in Phnom Penh making businesses achieve a quality online presence."
                     keywords="yeaksa, yeaksa digital, yeaksa digital works, yeaksadigital, yeaksa graphic design, yeaksa web development, yeaksa creative, yeaksa design agency, design cambodia, designers in cambodia, graphics design cambodia, graphic design in cambodia, cambodian graphic designer, grapics design phnom penh, phnom penh graphic designers, phnom penh creative agency, phnom penh design aegency, design agency, design agency cambodia, design agency phnom penh, esports creative agency, esports design agency, digital marketing in cambodia, digital marketing phnom penh, digital marketing agency, digital marketing in phnom penh, e-sports cambodia, cambodian esports, cambodia esports, cambodia professional esports organization, esports organization in cambodia, khmer esports organization, yeaksa, yeaksa esports, yeaksa esports organization, photographer in phnom penh, photographer in cambodia, cambodia photography, photography agency, photography freelance, graphics design freelance, graphic design freelance, frontend web development in cambodia, frontend web development in phnom penh, phnom penh web developer, phnom penh web development, photography agency"
                 /> 
                 :   <NextSeo 
                         title="YeakSa Digital Works"
-                        description="Design agency based in Phnom Penh with an Esports niche."
+                        description="Design agency based in Phnom Penh making businesses achieve a quality online presence."
                         keywords="yeaksa, yeaksa digital, yeaksa digital works, yeaksadigital, yeaksa graphic design, yeaksa web development, yeaksa creative, yeaksa design agency, design cambodia, designers in cambodia, graphics design cambodia, graphic design in cambodia, cambodian graphic designer, grapics design phnom penh, phnom penh graphic designers, phnom penh creative agency, phnom penh design aegency, design agency, design agency cambodia, design agency phnom penh, esports creative agency, esports design agency, digital marketing in cambodia, digital marketing phnom penh, digital marketing agency, digital marketing in phnom penh, e-sports cambodia, cambodian esports, cambodia esports, cambodia professional esports organization, esports organization in cambodia, khmer esports organization, yeaksa, yeaksa esports, yeaksa esports organization, photographer in phnom penh, photographer in cambodia, cambodia photography, photography agency, photography freelance, graphics design freelance, graphic design freelance, frontend web development in cambodia, frontend web development in phnom penh, phnom penh web developer, phnom penh web development, photography agency"
                     /> 
             }
