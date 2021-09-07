cc5d821a8b13796c9c1693dcfdea4b5b80a4d4ac
Masaya Shida <masayashida@gmail.com>
2021-09-07 23:02:02 +0700

    added new staff, fixed staffContainer

--- components/aboutTeam.js
+++ components/aboutTeam.js
@@ -10,7 +10,7 @@ export default function AboutTeam() {
             </Fade>
             <Fade bottom cascade>
                 <div className={styles.staffContainer}>
-                    <div className={styles.div1}>
+                    <div className={styles.staffContent}>
                         <Image src="/images/maya-no-bg_result.webp" width="300" height="300" alt="Masaya Shida" />
                         <span>
                             <h2>Shida Masaya</h2>
@@ -27,7 +27,7 @@ export default function AboutTeam() {
                             </a>
                         </span>
                     </div>
-                    <div className={styles.div2}>
+                    <div className={styles.staffContent}>
                         <Image src="/images/dekin-no-bg_result.webp" width="300" height="300" alt="Chao Sokyoukin" />
                         <span>
                             <h2>Chao Sokyoukin</h2>
@@ -44,7 +44,7 @@ export default function AboutTeam() {
                             </a>                               
                         </span>
                     </div>
-                    <div className={styles.div3}>
+                    <div className={styles.staffContent}>
                         <Image src="/images/soth-no-bg.webp" width="300" height="300" alt="Sokhon Keovisoth" />
                         <span>
                             <h2>Sokhon Keovisoth</h2>
@@ -56,18 +56,32 @@ export default function AboutTeam() {
                             <a href="https://t.me/sothss">
                                <Image src="/100w/telegram.png" width="80" height="80" alt="Telegram" />
                             </a>
-                            <a href="mailto:sokyoukin@gmail.com">
+                            <a href="mailto:keovisothsokhon@gmail.com">
                                <Image src="/100w/email.png" width="80" height="80" alt="Email" />
                             </a>
                         </span>
                     </div>
-                    {/* <div className={styles.div3}>
-                    
-                    </div> */}
+                    <div className={styles.staffContent}>
+                        <Image src="/images/JACK_result.webp" width="300" height="300" alt="Dekin Portrait" />
+                        <span>
+                            <h2>Prom Danit</h2>
+                            <small>
+                                Videographer and Video Editor
+                            </small>
+                        </span>
+                        <span className={styles.socialContainer}>
+                            <a href="https://t.me/jackkuy">
+                               <Image src="/100w/telegram.png" width="80" height="80" alt="Telegram" />
+                            </a>
+                            <a href="mailto:sunjack223344@gmail.com">
+                               <Image src="/100w/email.png" width="80" height="80" alt="Email" />
+                            </a>                               
+                        </span>
+                    </div>
+                </div>
                 <p className={styles.teamDescription}>
                     From the creative team behind <a href="https://yeaksa-esports.com">YeakSa Esports</a>, we aim to utilize our skills to help your business achieve a quality online presence.
                 </p>
-                </div>
             </Fade>
         </div>
     )
